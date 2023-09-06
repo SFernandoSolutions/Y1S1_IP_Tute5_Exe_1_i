fa6c65855f19eae452a07de23477e55c9a281142
SFernando Solutions <143948014+SFernandoSolutions@users.noreply.github.com>
2023-09-06 13:19:27 +0530

    Create exe.c

---
+++ exe.c
@@ -0,0 +1,25 @@
+//Write C programs to
+//i) Display numbers from 10 to 20.
+
+
+#include <stdio.h>
+int main(void){
+//	cretae variables
+	int num;
+	
+//	assign values to variable	
+	num=10;
+	
+//	loop
+	while (num<=20){
+		
+//		otput
+		printf("%d\n",num);
+		
+//		calculation
+		num=num+1;
+		
+	}
+	
+	return 0;
+}
